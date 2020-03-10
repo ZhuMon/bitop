bc02f98f81fd9e63da872ccf558be8a9dc0fa276
YuXiang <richardlin0212@gmail.com>
2020-03-11 01:32:51 +0800

    Use clang-format to maintain code
    Add .clang-format from git:sysprog21/fibdrv
    Format easy.c by clang-format

--- easy.c
+++ easy.c
@@ -1,25 +1,25 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    //
+    // unsigned number only
+    //
 
-  //
-  // unsigned number only
-  //
+    unsigned long x = 32;
 
-  unsigned long x = 32;
+    // x & (x-1)
+    //
+    // In unsigned number, x & (x-1) will delete the most right 1
+    //
+    // Check whether the number is the power of 2
+    //      x = 0101, x-1 = 0100, x & (x-1) = 0100 != 0
+    //      x = 0100, x-1 = 0011, x & (x-1) = 0000 == 0
 
-  // x & (x-1)
-  //
-  // In unsigned number, x & (x-1) will delete the most right 1
-  //
-  // Check whether the number is the power of 2
-  //      x = 0101, x-1 = 0100, x & (x-1) = 0100 != 0
-  //      x = 0100, x-1 = 0011, x & (x-1) = 0000 == 0
-
-  if ((x & (x - 1)) == 0) {
-    printf("x is 2^n\n");
+    if ((x & (x - 1)) == 0) {
+        printf("x is 2^n\n");
     }
 
     return 0;
